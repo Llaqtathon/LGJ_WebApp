86971bd34c60889effae48718110fbdc247c74bc
Rodrigo Lopez <64718167+Rodrigoalt15@users.noreply.github.com>
2022-04-29 04:53:54 -0500

    Update DistritoRepository.java

---
+++ LGJ_api/src/main/java/com/lgj/webapp/repository/DistritoRepository.java
@@ -0,0 +1,9 @@
+package com.lgj.webapp.repository;
+
+import com.lgj.webapp.entities.Distrito;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DistritoRepository extends JpaRepository<Distrito, Long>{
+    
+}
