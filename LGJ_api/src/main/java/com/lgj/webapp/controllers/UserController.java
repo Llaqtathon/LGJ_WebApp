e4dff3f88cc6a3a837eddbbd108c645f0732ddca
TomasNM229 <67438238+TomasNM229@users.noreply.github.com>
2022-05-01 19:38:00 -0500

    US04 #4
    Visualizacion del perfil

--- LGJ_api/src/main/java/com/lgj/webapp/controllers/UserController.java
+++ LGJ_api/src/main/java/com/lgj/webapp/controllers/UserController.java
@@ -1,8 +1,10 @@
 package com.lgj.webapp.controllers;
 
 import com.lgj.webapp.dto.InscripcionResponse;
+import com.lgj.webapp.dto.OrderResponse;
 import com.lgj.webapp.dto.UserDto;
 import com.lgj.webapp.entities.Inscripcion;
+import com.lgj.webapp.entities.Order;
 import com.lgj.webapp.entities.User;
 import com.lgj.webapp.services.UserService;
 //import com.lgj.webapp.util.EntityDtoConverter;
@@ -36,6 +38,12 @@ public class UserController {
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User request) {
         return new ResponseEntity<User>(userService.createUser(request), HttpStatus.CREATED);
+    }
+    @GetMapping("/{id}/{rol}")
+    public ResponseEntity<User> findParticipant(@PathVariable Long id, @PathVariable RolSelection rol) {
+        User user = userService.findById(id);
+        user.setRol(rol);
+        return new ResponseEntity<User>(userService.updateUser(user), HttpStatus.OK);
     }
     @PatchMapping("/{id}/{rol}")
     public ResponseEntity<User> updateUser(@PathVariable Long id, @PathVariable RolSelection rol, @RequestBody User userDetails) {
