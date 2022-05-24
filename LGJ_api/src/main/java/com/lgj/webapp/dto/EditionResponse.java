1c8f2f2ac62b3c39589173cb09e85565da3313b9
Maria Isabel Lopez <mrlpz1515@gmail.com>
2022-05-24 10:52:49 -0500

    US00 - Clean up a other things

---
+++ LGJ_api/src/main/java/com/lgj/webapp/dto/EditionResponse.java
@@ -0,0 +1,18 @@
+package com.lgj.webapp.dto;
+import java.util.Date;
+import java.util.List;
+
+import com.lgj.webapp.entities.MentorEdition;
+import lombok.Data;
+
+@Data
+public class EditionResponse {
+    private String name;
+    private String description;
+    private Date dateStart;
+    private Date dateEnd;
+    private String theme;
+    private String location;
+    private String locationUrlGmaps;
+    private List<MentorEdition> mentors;
+}
