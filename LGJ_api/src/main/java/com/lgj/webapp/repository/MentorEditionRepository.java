ee581c42fbe71050b2a779e068f3760726b87d53
Coralain <73152381+Coralain10@users.noreply.github.com>
2022-05-31 04:40:44 -0500

    US24 | Reduce query

--- LGJ_api/src/main/java/com/lgj/webapp/repository/MentorEditionRepository.java
+++ LGJ_api/src/main/java/com/lgj/webapp/repository/MentorEditionRepository.java
@@ -17,17 +17,7 @@ public interface MentorEditionRepository extends JpaRepository<MentorEdition, Lo
   List<Mentor> findMentorByEditionId(Long editionId);
   // @JsonIgnore
   // List<MentorEdition> findByEditionId(Long editionId);
-  String query_mentor_edition = "SELECT u.nombres, u.apellidos, u.rol, u.username, "
-    + "m.url_photo, e.name, e.date_start, e.date_end, "
-    + "e.date_start_preproduction, e.date_end_postproduction, "
-    + "ma.date_start, ma.date_end, me.* "
-    + "FROM mentor_edition me "
-    + "join mentor m on me.mentor_id = m.mentor_id "
-    + "join edicion e on me.edition_id = e.id "
-    + "join users u on m.mentor_id = u.id "
-    + "join mentor_availability ma "
-    + "on me.edition_id = ma.edition_id and me.mentor_id = ma.edition_id "
-    +" where me.edition_id = ?1";
+  String query_mentor_edition = "SELECT me.* FROM mentor_edition me where me.edition_id = ?1";
   @Query(value = query_mentor_edition, nativeQuery = true)
   List<MentorEdition> findByEditionId(Long editionId);
   @JsonIgnore
