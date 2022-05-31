c1f1c944a96f66723be5b7acc582b10c743931be
Maria Isabel Lopez <mrlpz1515@gmail.com>
2022-05-31 15:19:54 -0500

    PUT error

--- LGJ_api/src/main/java/com/lgj/webapp/config/CorsConfig.java
+++ LGJ_api/src/main/java/com/lgj/webapp/config/CorsConfig.java
@@ -25,7 +25,7 @@ public class CorsConfig implements Filter{
             HttpServletRequest request = (HttpServletRequest) req;
 
             response.setHeader("Access-Control-Allow-Origin", "*");
-            response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+            response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
             response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
             response.setHeader("Access-Control-Max-Age", "3600");
             response.setHeader("Access-Control-Allow-Credentials", "true");
